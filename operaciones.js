6c1cc73a3e8ac7a977561ee8c9376581dfff7560
Hugo Campos <hugo.camposc26@gmail.com>
2017-02-07 12:40:24 +0100

    Modificaciones
    Se importa el archivo JS con las operaciones al HTML con algunas
    modificaciones.

--- operaciones.js
+++ operaciones.js
@@ -1,30 +1,33 @@
-//Número máximo
-var max = 100;
+function generarNumero(){
 
-//Número mínimo
-var min = 1;
+	//Número máximo
+	var max = 100;
 
-//Número aleatorio
-var numero_secreto = Math.random() * (max-min)+min;
-//Convertimos el número a entero
-numero_secreto = parseInt(numero_secreto);
+	//Número mínimo
+	var min = 1;
 
-console.log(numero_secreto);
+	//Número aleatorio
+	var numero_secreto = Math.random() * (max-min)+min;
+	//Convertimos el número a entero
+	numero_secreto = parseInt(numero_secreto);
 
-var mensaje = "Introduce un número par adivinar el número mágico";
+	console.log(numero_secreto);
 
-while(true){
-	var numero_usuario = prompt(mensaje, "0");
-	numero_usuario = parseInt(numero_usuario);
+	var mensaje = "Introduce un número par adivinar el número mágico (El 0 finaliza el programa)";
 
-	if(numero_usuario === numero_secreto){
-		alert("¡Ganaste! Adivinaste el número secreto");
-		break;
-	}else if (numero_usuario === 0) {
-		break;
-	}else if (numero_usuario > numero_secreto) {
-		mensaje = "Lo sentimos, el número que ingresaste es mayor al número mágico";
-	}else if (numero_usuario < numero_secreto) {
-		mensaje = "Lo sentimos, el número que ingresaste es mayor al número mágico";
+	while(true){
+		var numero_usuario = prompt(mensaje, "0");
+		numero_usuario = parseInt(numero_usuario);
+
+		if(numero_usuario === numero_secreto){
+			alert("¡Ganaste! Adivinaste el número secreto");
+			break;
+		}else if (numero_usuario === 0) {
+			break;
+		}else if (numero_usuario > numero_secreto) {
+			mensaje = "Lo sentimos, el número que ingresaste es mayor al número mágico";
+		}else if (numero_usuario < numero_secreto) {
+			mensaje = "Lo sentimos, el número que ingresaste es menor al número mágico";
+		}
 	}
 }
